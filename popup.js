687fd8c255bf6ca5600ba4df61a743949f98a259
Chahana Reddy <chahanareddy18@gmail.com>
2025-03-09 22:16:37 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { 
   const goalContainer = document.getElementById("goalContainer");
   const timerContainer = document.getElementById("timerContainer");
 
